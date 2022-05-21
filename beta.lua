50d5fb8c0c121393be74e0fd9c332bd4edcc4f0c
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 04:26:34 -0400

    Create beta.lua

---
+++ beta.lua
@@ -0,0 +1,15 @@
+
+game.StarterGui:SetCore(
+    "SendNotification",
+    {
+	Title = "Neverlackin BETA 0.1v",
+	Text = "a admin.",
+}
+)
+local main1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local main2 = main1.CreateLib("Neverlockin", "DarkTheme")
+local main3 = main2:NewTab("Legitbot")
+local main4 = main3:NewSection("streamproof")
+Section:NewButton("steamproof silent", "low fov ( 15 )", function()
+
+end)
