4d4ba1141bf7135690e84cf813261767079a8eca
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 05:00:16 -0400

    Update beta.lua

--- beta.lua
+++ beta.lua
@@ -5,6 +5,7 @@ game.StarterGui:SetCore(
 	Title = "Neverlackin BETA 0.1v",
 	Text = "Thanks forbuying Neverlackin",
 		}
+game.StarterGui:SetCore(
     "SendNotification",
     {
 	Title = "Whitelist",
