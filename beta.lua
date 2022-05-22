bde7ea746f83c892ac13f40678bb066da645e7ce
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 23:12:39 -0400

    Update beta.lua

--- beta.lua
+++ beta.lua
@@ -133,16 +133,88 @@ main12:NewButton("Armour", "Buys armour", function()
 	plr.Character.HumanoidRootPart.CFrame = CFrame.new(savedarmourpos)
 end)
 main12:NewButton("Rev", "Buys Rev", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/autobuy/main/rev.lua"))()
+    _G.savedhumanoidpos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+				pcall(function()
+					for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+						if v.Name == '[Revolver] - $1300' then
+							v.Parent = game.Players.LocalPlayer.Character
+						end
+					end
+				end)
+				wait(.9)
+				local targetpos = CFrame.new(-867.581482, -32.6492004, -531.12439)
+				local plr = game.Players.LocalPlayer
+				local pos = plr.Character.HumanoidRootPart.Position
+				if not game.Players.LocalPlayer.Character:FindFirstChild("[Revolver] - $1300") then
+					wait(.4)
+					plr.Character.HumanoidRootPart.CFrame = targetpos
+					local cd = game:GetService("Workspace").Ignored.Shop["[Revolver] - $1300"]:FindFirstChild("ClickDetector")
+					wait(.9)
+					fireclickdetector(cd)
+					wait(.9)
+					fireclickdetector(cd)
+					wait(.4)
+				end
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(_G.savedhumanoidpos)
 end)
 main12:NewButton("Rev ammo", "Buys Rev ammo", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/autobuy/main/reva.lua"))()
+    				_G.savedhumanoidpos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+				pcall(function()
+					for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+						if v.Name == '12 [Revolver Ammo] - $75' then
+							v.Parent = game.Players.LocalPlayer.Character
+						end
+					end
+				end)
+				wait(.9)
+				local targetpos = CFrame.new(-867.581482, -32.6492004, -531.12439)
+				local plr = game.Players.LocalPlayer
+				local pos = plr.Character.HumanoidRootPart.Position
+				if not game.Players.LocalPlayer.Character:FindFirstChild("12 [Revolver Ammo] - $75") then
+					plr.Character.HumanoidRootPart.CFrame = targetpos
+					local cd = game:GetService("Workspace").Ignored.Shop["12 [Revolver Ammo] - $75"]:FindFirstChild("ClickDetector")
+					wait(.9)
+					fireclickdetector(cd)
+					wait(.9)
+					fireclickdetector(cd)
+					wait(.4)
+				end
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(_G.savedhumanoidpos)
 end)
 main12:NewButton("DoubleBarrel", "Buys Double Barrel", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/autobuy/main/db.lua"))()
+    				_G.savedhumanoidpos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+				game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1039.53931, 21.75, -258.733154)
+				wait(.5)
+
+				fireclickdetector(game.Workspace.Ignored.Shop['[Double-Barrel SG] - $1400'].ClickDetector)
+				fireclickdetector(game.Workspace.Ignored.Shop['[Double-Barrel SG] - $1400'].ClickDetector)
+				wait()
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(_G.savedhumanoidpos)
 end)
 main12:NewButton("DoubleBarrel ammo", "Buys Double Barrel ammo", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/neverlackinga/autobuy/main/db1.lua"))()
+    	_G.savedhumanoidpos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+				pcall(function()
+					for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+						if v.Name == '18 [Double-Barrel SG Ammo] - $60' then
+							v.Parent = game.Players.LocalPlayer.Character
+						end
+					end
+				end)
+				wait(.9)
+				local targetpos = CFrame.new(-1046.20032, 18.8513641, -256.449951, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+				local plr = game.Players.LocalPlayer
+				local pos = plr.Character.HumanoidRootPart.Position
+				if not game.Players.LocalPlayer.Character:FindFirstChild("18 [Double-Barrel SG Ammo] - $60") then
+					plr.Character.HumanoidRootPart.CFrame = targetpos
+					local cd = game:GetService("Workspace").Ignored.Shop["18 [Double-Barrel SG Ammo] - $60"]:FindFirstChild("ClickDetector")
+					wait(.9)
+					fireclickdetector(cd)
+					wait(.9)
+					fireclickdetector(cd)
+					wait(.4)
+				end
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(_G.savedhumanoidpos)
 end)
 local main14 = main2:NewTab("Misc")
 local main15 = main14:NewSection("Menu key")
