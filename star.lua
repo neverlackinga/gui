c2a200733032930d2073cbd1f387d91da71ee4e4
bubatz <71530056+cynhook@users.noreply.github.com>
2022-06-21 01:59:56 -0400

    Delete star.lua

--- star.lua
+++
@@ -1,8 +0,0 @@
-game.Players.PlayerAdded:Connect(function(plr) 
-	plr.CharacterAdded:Connect(function(Char)
-		Char:WaitForChild("Humanoid")
-		if plr.UserId == 457621896, then -- You may change to any player id.
-			Char.Humanoid.DisplayName = "[💸]" .. plr.DisplayName -- Any emoji.
-		end
-	end)
-end)
